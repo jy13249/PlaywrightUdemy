431d9e0edcaecc4087d4163b31a9eb0605ee9dea
Justin Wyman <JWyman@tql.com>
2023-12-08 14:44:28 -0500

    final restructure

---
+++ JS Fundamentals/Lessons/lesson1.js
@@ -0,0 +1,25 @@
+//1. Hello World
+console.log("Hello World")
+
+// Variables
+var firstName = "John"
+let lastName = "Smith"
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
+age = "6"
+console.log(age)
+
+//constants
+const occupation = "Engineer"
+console.log(occupation)
+
+//data types
+var middleName = "David" //string
+var ageofBrother = 25 //number
+var isHeMarried = false //boolean
+var yearsInMarriage = null //null
+var numberofCars = undefined //undefined
