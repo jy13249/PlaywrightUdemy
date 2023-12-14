1c9af7916ba9e6cba6d2d72a0780b8c262d41fbf
Justin Wyman <jy13249@gmail.com>
2023-12-14 09:38:47 -0500

    finished lesson 3

--- JS Fundamentals/Lessons/lesson3.js
+++ JS Fundamentals/Lessons/lesson3.js
@@ -1,1 +1,27 @@
-//
+//objects
+
+var customer = {
+    firstName: "John",
+    lastName: "Smith",
+    cars: ["Volvo", "Toyota", "Tesla"]
+}
+
+console.log(customer)
+console.log(customer.firstName)
+console.log(customer["lastName"])
+
+
+//Dot notation
+customer.firstName = "Mike"
+
+//Bracket notation
+customer.lastName = "Silver"
+
+console.log(`${customer.firstName} ${customer.lastName}`)
+
+//arrays
+var car = ["Volvo", "Toyota", "Tesla"]
+car[1] = "BMW"
+console.log(car[0])
+console.log(car[1])
+console.log(customer.cars[0])
