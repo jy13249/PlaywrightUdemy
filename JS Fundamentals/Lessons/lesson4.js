0e9d7b9e847b9d629fd9c677696e3166c8e75fce
Justin Wyman <jy13249@gmail.com>
2023-12-14 10:55:33 -0500

    finished lesson 4

---
+++ JS Fundamentals/Lessons/lesson4.js
@@ -0,0 +1,16 @@
+// Relational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than equal
+// <= - less than equal
+
+var result = 10 > 5
+console.log(result)
+
+// Equality operators
+
+var x = 1
+console.log(x == "1") //loose comparison(comparing value)
+console.log(x === "1") //strict comparison(comparing value and data type of value)
+console.log(x === 1)
