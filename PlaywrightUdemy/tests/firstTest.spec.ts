5fea3376964792b022dd1a435897c064773297a4
Justin Wyman <JWyman@tql.com>
2023-12-27 15:53:35 -0500

    finished hooks and flow control lesson

--- PlaywrightUdemy/tests/firstTest.spec.ts
+++ PlaywrightUdemy/tests/firstTest.spec.ts
@@ -1,9 +1,28 @@
 import {expect, test} from '@playwright/test';
 
-test('the first test', async ({page}) => {
+
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
-    await expect(page.getByText('Inline form')).toBeVisible()
 })
 
+test.describe('suite1', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByRole('link', { name: 'Charts', exact: true }).click()
+    })
+    
+    test('the first test', async ({page}) => {
+    await page.getByText('Echarts').click()
+    })
+})
+
+test.describe('suite2', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Forms').click()
+    })
+    
+    test('the first test', async ({page}) => {
+    await page.getByText('Form Layouts').click()
+    await expect(page.getByText('Inline form')).toBeVisible()
+    })
+
+})
