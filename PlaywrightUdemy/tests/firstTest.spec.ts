5a93e9c7765bbb349112123bc071ce34e032933d
Justin Wyman <JWyman@tql.com>
2024-01-10 15:48:14 -0500

    finished assertions

--- PlaywrightUdemy/tests/firstTest.spec.ts
+++ PlaywrightUdemy/tests/firstTest.spec.ts
@@ -77,3 +77,56 @@ test('locating parent elements', async ({page}) => {
     .getByRole('textbox', {name: 'Email'}).click()
     await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: 'Email'}).click()
 })
+
+test('Reusing the locators', async ({page}) => {
+    const basicForm = page.locator('nb-card').filter({hasText: 'Basic form'})
+    const emailField = basicForm.getByRole('textbox', {name: 'Email'})
+
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', {name: 'Password'}).fill('Welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+})
+
+test('Extracting values', async({page}) => {
+    //single text value
+    const basicForm = page.locator('nb-card').filter({hasText: 'Basic form'})
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    //all text values
+    const allRadioButtonsText = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsText).toContain('Option 1')
+
+    //input value
+    const emailField = basicForm.getByRole('textbox', {name: 'Email'})
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    //attribute value
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual('Email')
+
+})
+
+test('Assertions', async ({page}) => {
+    const basicFormButton = page.locator('nb-card').filter({hasText: 'Basic form'}).locator('button')
+    //General assertions
+    const value = 5
+    expect(value).toEqual(5)
+
+    const text = await basicFormButton.textContent()
+    expect(text).toEqual('Submit')
+
+    //locator assertions
+    await expect(basicFormButton).toHaveText('Submit')  
+    
+    //Soft assertion(continues test regardless of failure)
+    await expect.soft(basicFormButton).toHaveText('Submit')
+    await basicFormButton.click()
+
+})
