9d10cea2ae772c5a4e7e0f3cbd9d95e7a171b8cd
Justin Wyman <JWyman@tql.com>
2024-01-25 09:58:03 -0500

    finished tooltip lesson

--- PlaywrightUdemy/tests/uiComponents.spec.ts
+++ PlaywrightUdemy/tests/uiComponents.spec.ts
@@ -113,4 +113,16 @@ test('lists and dropdowns', async ({page}) => {
     }
 })
 
+test('tooltips', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const tooltipCard = page.locator('nb-card', {hasText: 'Tooltip Placements'})
+    await tooltipCard.getByRole('button', {name: 'Top'}).hover()
+
+    page.getByRole('tooltip') //if you have a role tooltip created
+    const toolTip = await page.locator('nb-tooltip').textContent()
+    expect(toolTip).toEqual('This is a tooltip')
+})
+
 
